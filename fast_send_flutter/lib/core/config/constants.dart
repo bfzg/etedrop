31a00fc4f7af100635ae2a11751d44696faf3290
yuan <yuanzhou_cn@qq.com>
2026-03-29 20:35:04 +0800

    修改服务地址

--- fast_send_flutter/lib/core/config/constants.dart
+++ fast_send_flutter/lib/core/config/constants.dart
@@ -4,16 +4,16 @@ class AppConstants {
 
   /// REST API 基础地址（如需要调用服务端 HTTP 接口）
   // static const String apiBaseUrl = 'http://43.153.143.37:40321';
-  static const String apiBaseUrl = 'http://192.168.1.7:3000';
+  static const String apiBaseUrl = 'http://192.168.1.9:3000';
 
   /// 信令服务器地址（WebRTC 文件传输用）
   // static const String signalingServerUrl = 'wss://fastsend.ing/api/connect';
   // static const String signalingServerUrl = 'ws://43.153.143.37:40321/api/connect';
-  static const String signalingServerUrl = 'ws://192.168.1.7:3000/api/connect';
+  static const String signalingServerUrl = 'ws://192.168.1.9:3000/api/connect';
 
   /// 设备管理 WebSocket 地址（分享功能用）
   // static const String shareServerUrl = 'ws://43.153.143.37:40321/api/share';
-  static const String shareServerUrl = 'ws://192.168.1.7:3000/api/share';
+  static const String shareServerUrl = 'ws://192.168.1.9:3000/api/share';
 
   /// 应用名称
   static const String appName = 'Eddy';
